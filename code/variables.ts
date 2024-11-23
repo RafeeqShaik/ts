f2b7395e6519b0bfc45bfc24c7cf21c0fd1be8a2
Rafeeq Shaik <rafeeq.ahamed@sagunaconsulting.com>
2024-11-23 23:35:48 +0530

    variables, rules & community code practices

---
+++ code/variables.ts
@@ -0,0 +1,52 @@
+/**
+ * Rules to create variables
+ * 1. Variable name should start with a letter
+ * 2. Variable name should not contain any special characters except _
+ * 3. Variable name should not contain any spaces
+ * 4. Variable name should not start with a numbers
+ * 5. Variable name should not be a reserved keyword
+ */
+
+/**
+ * Type of data
+ * 1. primitive data types
+ * 2. complex data types
+ *
+ * Primitive data types
+ * 1. string
+ * 2. number
+ * 3. boolean
+ * 4. null
+ * 5. undefined
+ */
+
+let _name = 'Kedar told me that, "go get some sleep bruv". ';
+let age = 26;
+let age2 = "26";
+
+let firstName = "Kedar Eswar";
+let lastName = "Garigipati";
+let fullName = `My full name is ${firstName} ${lastName}`;
+let worstName = "My full name is" + " " + firstName + " " + lastName;
+let isMale = true;
+let isRaining = false;
+
+let users = null;
+
+let count;
+console.log(count);
+
+count = 10;
+
+console.log(count);
+
+count = 20;
+console.log(count);
+
+count = undefined;
+console.log(count);
+
+// camelcase notation for variables, functions
+// pascal notation for classes
+let isGood = true;
+let IsGood = false;
